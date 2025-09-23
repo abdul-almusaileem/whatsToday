7d30ee566ee59f2994afdc79ed73f0a5017175e6
abdul-almusaileem <7amanga@gmail.com>
2025-09-23 14:15:38 +0300

    Initial Commit

---
+++ whatsTodayTests/whatsTodayTests.swift
@@ -0,0 +1,17 @@
+//
+//  whatsTodayTests.swift
+//  whatsTodayTests
+//
+//  Created by Abdulrahman Almusaileem on 23/09/2025.
+//
+
+import Testing
+@testable import whatsToday
+
+struct whatsTodayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
